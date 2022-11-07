01b98c3d0e96ea50185b0da892fe6a23cf9083a4
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2022-11-07 16:13:52 -0300

    Validações

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    belongs_to :category
+    validates :name, presence: true
+    validates :author, presence: true
+    validates :desc, presence: true, length: { minimum: 10 }
+    validates :isbn, presence: true, length: { is: 13 }
 end
