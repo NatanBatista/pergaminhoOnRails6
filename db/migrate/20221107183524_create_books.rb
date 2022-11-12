b7b478b6dd8456b07f16922fd986d06f0b122702
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2022-11-12 16:53:49 -0300

    Campos UNIQUE db

--- db/migrate/20221107183524_create_books.rb
+++ db/migrate/20221107183524_create_books.rb
@@ -9,5 +9,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :books, :name,                unique: true
+    add_index :books, :isbn,                unique: true
   end
 end
