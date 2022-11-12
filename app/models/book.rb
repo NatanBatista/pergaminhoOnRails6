1892c52d3e5e76376fa5167990465b4a4d4a6590
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2022-11-12 16:52:37 -0300

    Validação de campo unique

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
     belongs_to :category
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
     validates :author, presence: true
     validates :desc, presence: true, length: { minimum: 10 }
-    validates :isbn, presence: true, length: { is: 13 }
+    validates :isbn, presence: true, length: { is: 13 }, uniqueness: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     has_many :books, dependent: :restrict_with_error
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
 end
