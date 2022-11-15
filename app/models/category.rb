b73ccd84c09493d5a997238baf7e2bfb36ecb484
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2022-11-14 21:52:11 -0300

    Validação de tamanho do campo name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     has_many :books, dependent: :restrict_with_error
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
